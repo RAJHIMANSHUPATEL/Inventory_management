edc11b58416d9935d14f81dbe88f94f46e733d00
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2025-06-08 13:04:40 +0530

    deploying

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://noah:noahXsaviour@cluster0.qxtvhpy.mongodb.net/?retryWrites=true&w=majority&appName=InventoryManagement")
+mongoose.connect(process.env.MONGO_URI)
 .then(console.log("DB connection successful"))
 .catch((error)=> {
     console.log(error)
